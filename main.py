e35b6f64b7f7f4fb86d70cd114a5ea2680c849fa
Gankydang <108336379+Gankydang@users.noreply.github.com>
2022-08-01 15:56:38 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-distance = 0
-
+Moisture = 0
+distance2 = 0
 def on_forever():
-    Moisture = pins.analog_read_pin(AnalogPin.P2)
-    distance = grove.measure_in_centimeters_v2(DigitalPin.P0)
-    basic.show_number(distance)
-    if Moisture < 10:
-        pins.digital_write_pin(DigitalPin.P1, 1)
-    else:
-        pins.digital_write_pin(DigitalPin.P1, 0)
-basic.forever(on_forever)
+    global Moisture, distance2
+    while True:
+        Moisture = pins.analog_read_pin(AnalogPin.P2)
+        distance2 = grove.measure_in_centimeters_v2(DigitalPin.P0)
+        if Moisture < 30:
+            pins.digital_write_pin(DigitalPin.P1, 1)
+        else:
+            pins.digital_write_pin(DigitalPin.P1, 0)
+        if distance2 < 2.1:
+            basic.show_icon(IconNames.YES)
+        else:
+            basic.show_icon(IconNames.NO)
+        
+def on_button_pressed_a():
+    on_forever()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
-let distance = 0
-basic.forever(function on_forever() {
-    let Moisture = pins.analogReadPin(AnalogPin.P2)
-    let distance = grove.measureInCentimetersV2(DigitalPin.P0)
-    basic.showNumber(distance)
-    if (Moisture < 10) {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-    }
+let Moisture = 0
+let distance2 = 0
+function on_forever() {
     
+    while (true) {
+        Moisture = pins.analogReadPin(AnalogPin.P2)
+        distance2 = grove.measureInCentimetersV2(DigitalPin.P0)
+        if (Moisture < 30) {
+            pins.digitalWritePin(DigitalPin.P1, 1)
+        } else {
+            pins.digitalWritePin(DigitalPin.P1, 0)
+        }
+        
+        if (distance2 < 2.1) {
+            basic.showIcon(IconNames.Yes)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+        
+    }
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    on_forever()
 })
