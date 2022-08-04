2be839776f6236fb9e39e4eb09c9ecf6b9d937c2
Gankydang <108336379+Gankydang@users.noreply.github.com>
2022-08-04 10:09:12 +0800

    Update pxt.json, main.ts, main.py, cutting.ts

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 Moisture = 0
 distance2 = 0
+a_pressed = False
+b_pressed = False
 def on_forever():
     global Moisture, distance2
     while True:
         Moisture = pins.analog_read_pin(AnalogPin.P2)
         distance2 = grove.measure_in_centimeters_v2(DigitalPin.P0)
-        if Moisture < 30:
-            pins.digital_write_pin(DigitalPin.P1, 1)
-        else:
+        if input.button_is_pressed(Button.A):
+            a_pressed = True
+            b_pressed = False
+        elif input.button_is_pressed(Button.B):
+            b_pressed = True
+            a_pressed = False
+        if a_pressed:
+            if Moisture < 30:
+                pins.digital_write_pin(DigitalPin.P1, 1)
+            else:
+                pins.digital_write_pin(DigitalPin.P1, 0)
+            if distance2 < 2.1:
+                basic.show_icon(IconNames.YES)
+            else:
+                basic.show_icon(IconNames.NO)
+        elif b_pressed:
+            basic.clear_screen()
             pins.digital_write_pin(DigitalPin.P1, 0)
-        if distance2 < 2.1:
-            basic.show_icon(IconNames.YES)
-        else:
-            basic.show_icon(IconNames.NO)
         
-def on_button_pressed_a():
-    on_forever()
-input.on_button_pressed(Button.A, on_button_pressed_a)
+
+on_forever()
 
--- main.ts
+++ main.ts
@@ -1,25 +1,41 @@
 let Moisture = 0
 let distance2 = 0
+let a_pressed = false
+let b_pressed = false
 function on_forever() {
+    let a_pressed: boolean;
+    let b_pressed: boolean;
     
     while (true) {
         Moisture = pins.analogReadPin(AnalogPin.P2)
         distance2 = grove.measureInCentimetersV2(DigitalPin.P0)
-        if (Moisture < 30) {
-            pins.digitalWritePin(DigitalPin.P1, 1)
-        } else {
-            pins.digitalWritePin(DigitalPin.P1, 0)
+        if (input.buttonIsPressed(Button.A)) {
+            a_pressed = true
+            b_pressed = false
+        } else if (input.buttonIsPressed(Button.B)) {
+            b_pressed = true
+            a_pressed = false
         }
         
-        if (distance2 < 2.1) {
-            basic.showIcon(IconNames.Yes)
-        } else {
-            basic.showIcon(IconNames.No)
+        if (a_pressed) {
+            if (Moisture < 30) {
+                pins.digitalWritePin(DigitalPin.P1, 1)
+            } else {
+                pins.digitalWritePin(DigitalPin.P1, 0)
+            }
+            
+            if (distance2 < 2.1) {
+                basic.showIcon(IconNames.Yes)
+            } else {
+                basic.showIcon(IconNames.No)
+            }
+            
+        } else if (b_pressed) {
+            basic.clearScreen()
+            pins.digitalWritePin(DigitalPin.P1, 0)
         }
         
     }
 }
 
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    on_forever()
-})
+on_forever()
